933052b488772c269af0e844d1ddcf83819e13de
Lucas <gomez-lucas@live.com.ar>
2020-03-25 16:45:41 -0300

    Code Refactored

--- app.js
+++ app.js
@@ -1,249 +1,66 @@
-
 /* Defino Variables */
-let array = []
-let filter = document.querySelector('.filter')
-let container = document.querySelector('.container')
-let filterList = document.querySelector('.filter__options')
-let response = ''
-let newArray = []
-let arrayObj = []
+let filterArray = []
+let filtertDiv = document.querySelector('.filter')
+let containerPostsDiv = document.querySelector('.container')
+let filterUl = document.querySelector('.filter__options')
+let jobPostArray = []
+let jobPostArrayFiltered = []
+
 
 /* Luego de Cargar la pagina*/
 document.addEventListener("DOMContentLoaded", async function () {
-    var data = './data.json'
-    const fetchResult = await fetch(data)
-    response = await fetchResult.json()
-    await printData(response)
+    const dataJson = './data.json'
+    const JobPostResponseJson = await fetch(dataJson)
+    const JobPostResponse = await JobPostResponseJson.json()
+    jobPostArray = await formatJobPostResponse(JobPostResponse)
+    await showJobPosts(jobPostArray)
 });
 
 
-/* Manejo del Array con los datos filtrados*/
-checkArray = (key, data) => {
-    /* Chequeo que el dato seleccionado no este repetido*/
-    if (array.filter(value => value[key] == data).length === 0) {
-        filter.classList.add('filter--show')
-        let object = { [key]: data }
-        array.push(object)
 
-        /*Una vez aparecido el filtro le agrego el evento al boton Clear*/
-        document.querySelector('.button__clear').addEventListener('click', function () {
-            filter.classList.remove('filter--show')
-            printData(response)
-            filterList.innerHTML = ''
-            array.length = 0
-            newArray.length = 0
-        })
-        /*Creo el elemento Li y Button del Filtro*/
-        let li = document.createElement('li')
-        li.setAttribute(key, data)
-        let button = document.createElement('button')
-        button.classList.add('filter__closeBTN')
-        li.innerHTML = `${data}`
-        button.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
-        <path fill="#FFF" fill-rule="evenodd"
-        d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
-        </svg>
-        `
-        /*Agrego el eventListener al button*/
-        button.addEventListener('click', function (e) {
-            handleCloseBtn(e)
-            printData(newArray)
+/* Dentro del objeto: join de los Array languajes y tools,
+{... "languages": ["HTML", "JavaScript"], "tools": ["React"]  
+      = "skills" : ["HTML", "JavaScript","React"]
+*/
+formatJobPostResponse = (jobPosts) => {
+    jobPosts.forEach(job => {
+        if (job.hasOwnProperty('languages') && job.hasOwnProperty('tools')) {
+            job.skills = [...job.languages, ...job.tools]
+        } else if (job.hasOwnProperty('languages')) {
+            job.skills = [...job.languages]
 
-        })
-        /*Agrego al documento*/
-        li.appendChild(button)
-        filterList.appendChild(li)
-    }
-}
-
-/* Funcion  relacionada con el button del cierre del filtro*/
-handleCloseBtn = (event) => {
-    let data, key
-    /*Switch detecta donde se hizo el click*/
-    switch (event.srcElement.tagName) {
-        case 'path':
-            event.target.parentNode.parentNode.parentNode.remove()
-            data = event.target.parentNode.parentNode.parentNode.attributes[0].value
-            key = event.target.parentNode.parentNode.parentNode.attributes[0].name
-            break;
-        case 'svg':
-            event.target.parentNode.parentNode.remove()
-            data = event.target.parentNode.parentNode.attributes[0].value
-            key = event.target.parentNode.parentNode.attributes[0].name
-
-            break;
-        case 'BUTTON':
-            event.target.parentNode.remove()
-            data = event.target.parentNode.attributes[0].value
-            key = event.target.parentNode.attributes[0].name
-            break;
-        default:
-            break;
-    }
-
-    /*Filtro y Elimino del ArrayFiltro la seleccion */
-    array = array.filter(item => data !== item[key])
-
-    let prueba = []
-    let mainKey
-    let arrayLanguagesTools = []
-    let role = ''
-    let level = ''
-
-    /*Recorro el Array Filtro para separar datos, en un array con lenguajes y tools, y variables para role y level*/
-    array.forEach(itemArray => {
-        for (key in itemArray) {
-            mainKey = key
-        }
-        if (mainKey === "data-languages" || mainKey === "data-tools") {
-            arrayLanguagesTools.push(itemArray[mainKey])
-        } else if (mainKey === "data-role") {
-            role = itemArray[mainKey]
+        } else if (job.hasOwnProperty('tools')) {
+            job.skills = [...job.tools]
         } else {
-            level = itemArray[mainKey]
+            job.skills = []
         }
+        delete job.languages
+        delete job.tools
     })
+    return jobPosts
 
-    /*Recorro el json Original*/
-    response.forEach(item => {
-        let isRole = false
-        let isLevel = false
-        let isExtra = false
-
-        /* Si el dato filtro existe y coincide con el del json True, si no existe tb True*/
-        if (role) {
-            if (role === (item.role.toLowerCase())) {
-                isRole = true
-            }
-        } else {
-            isRole = true
-        }
-
-        if (level) {
-            if (level === (item.level.toLowerCase())) {
-                isLevel = true
-            }
-        } else {
-            isLevel = true
-        }
-
-        /* Join de los Array de los datos */
-        let lowerCaseArr = item.hasOwnProperty('languages') && item.languages.map(language => language.toLowerCase())
-        let lowerCaseArr1 = item.hasOwnProperty('tools') && item.tools.map(tool => tool.toLowerCase())
-        let lower
-
-        if (lowerCaseArr && lowerCaseArr1) {
-            lower = [...lowerCaseArr, ...lowerCaseArr1]
-        } else if (lowerCaseArr) {
-            lower = [...lowerCaseArr]
-        } else {
-            lower = [...lowerCaseArr1]
-        }
-
-
-        /* Chequeo que el array del filtro este en cada uno de los datos provenientes del json, devuelvo true*/
-        if (lower && arrayLanguagesTools) {
-            if (arrayLanguagesTools.every(language => lower.includes(language))) {
-                isExtra = true
-            } else {
-                isExtra = false
-            }
-        } else {
-
-        }
-
-        /* paso por las pruebas, agrego el item del json al Array*/
-        if ((isRole && isLevel && isExtra)) {
-            prueba.push(item)
-        }
-    })
-
-
-    /* Variable que se rendea al documento, borro su info, y agrego la salida de lo anterio*/
-    newArray.length = 0
-    newArray = [...prueba]
-
-
-    /*Si no tengo mas datos en el array, lo elimino del documento*/
-    if (array.length === 0) {
-        filter.classList.remove('filter--show')
-        newArray = [...response]
-    }
-}
-
-
-
-/* Funcion que renderiza la info al documento dependiendo del filtro seleccionado*/
-updateScreen = (key, data) => {
-    let keyString = key.substr(5)
-    if (newArray.length === 0) {
-        response.forEach(item => {
-
-            switch (keyString) {
-                case 'role':
-                case 'level':
-                    if (item[keyString] && item[keyString].toLowerCase() === data) {
-                        newArray.push(item)
-                    }
-                    break;
-                case 'languages':
-                case 'tools':
-                    if (item[keyString]) {
-                        let newItem = item[keyString].map(item => item.toLowerCase())
-
-                        if (newItem.includes(data)) {
-                            newArray.push(item)
-                        }
-                    }
-                    break;
-                default:
-                    break;
-            }
-        })
-    } else {
-        /* Clickeando un segundo y + filtro */
-        let secondArr
-        switch (keyString) {
-            case 'role':
-            case 'level':
-                secondArr = newArray.filter(item => item[keyString] && item[keyString].toLowerCase() === data)
-                newArray.length = 0
-                newArray = [...secondArr]
-                break;
-            case 'languages':
-            case 'tools':
-                secondArr = newArray.filter(item => {
-                    let newItem = item[keyString] && item[keyString].map(it => it.toLowerCase())
-                    return newItem ? newItem.includes(data) : false
-                })
-                newArray.length = 0
-                newArray = [...secondArr]
-
-                break;
-            default:
-                break;
-        }
-
-    }
-}
-
-
-/*funcion que agrega el evento Listener a los Li de los Post*/
-addEventListenerLi = (li) => {
-    li.addEventListener('click', function (event) {
-        let key = event.target.attributes[0].name
-        let data = event.target.attributes[0].value.toLowerCase()
-        checkArray(key, data)
-        updateScreen(key, data)
-        printData(newArray)
-    })
 }
 
 /* Funcion que renderiza la info al documento*/
-printData = (jobs) => {
-    container.innerHTML = ''
-    jobs.map(job => {
+/*jobsPosts = [{
+{
+    "id": 1,
+    "company": "Photosnap",
+    "img": "./images/photosnap.svg",
+    "isItNew": true,
+    "isItFeatured": true,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "timeStamp": "1d ago",
+    "hours": "FullTime",
+    "location": "USA Only",
+    "skills": ["HTML", "CSS", "JavaScript"]
+  },
+}]*/
+showJobPosts = (jobPosts) => {
+    containerPostsDiv.innerHTML = ''
+    jobPosts.map(job => {
         let div = document.createElement('div')
 
         div.innerHTML = `
@@ -276,50 +93,225 @@ printData = (jobs) => {
          </ul>
       </div>
                 `
-        /* Agrego los Li filstro para seleccion y sus eventos Listeners a los Posts */
-        container.appendChild(div)
+        containerPostsDiv.appendChild(div)
 
-        let liRole = document.createElement('li')
-        //liRole.innerHTML = `<li data-role=${job.role} class="item__require"> ${job.role}  </li>`
-        liRole.dataset.role = `${job.role}`
-        liRole.classList.add("item__require")
-        liRole.innerHTML = `${job.role}`
-        addEventListenerLi(liRole)
-        document.querySelector(`#N${job.id}`).appendChild(liRole)
+        createPostLi("role", job.role, job.id)
+        createPostLi("level", job.level, job.id)
+        createPostLi("skills", job.skills, job.id)
+    })
+}
 
-        let liLevel = document.createElement('li')
-        // liLevel.innerHTML = `<li data-level=${job.level} class="item__require"> ${job.level} </li>`
-        liLevel.dataset.level = `${job.level}`
-        liLevel.classList.add("item__require")
-        liLevel.innerHTML = `${job.level}`
-        addEventListenerLi(liLevel)
-        document.querySelector(`#N${job.id}`).appendChild(liLevel)
-
-        if (job.languages && job.languages.length > 0) {
-            job.languages.forEach(language => {
-                let lilanguage = document.createElement('li')
-                lilanguage.dataset.languages = `${language}`
-                lilanguage.classList.add("item__require")
-                lilanguage.innerHTML = `${language}`
-              //    lilanguage.innerHTML = `<li data-languages=${language} class="item__require"> ${language}  </li>`
-                addEventListenerLi(lilanguage)
-                document.querySelector(`#N${job.id}`).appendChild(lilanguage)
+/* Agrega  Li con los filtros al Post  */
+createPostLi = (property, data, id) => {
+    let li = ''
+    if (property !== "skills") {
+        li = document.createElement('li')
+        li.dataset[property] = `${data}`
+        li.classList.add("item__require")
+        li.innerHTML = `${data}`
+        addEventListenerPostLi(li)
+        document.querySelector(`#N${id}`).appendChild(li)
+    } else {
+        if (data.length > 0) {
+            data.forEach(skill => {
+                li = document.createElement('li')
+                li.dataset[property] = `${skill}`
+                li.classList.add("item__require")
+                li.innerHTML = `${skill}`
+                addEventListenerPostLi(li)
+                document.querySelector(`#N${id}`).appendChild(li)
             })
         }
-        if (job.tools && job.tools.length > 0) {
-            job.tools.forEach(tool => {
-                let liTool = document.createElement('li')
-               liTool.dataset.tools = `${tool}`
-               liTool.classList.add("item__require")
-               liTool.innerHTML = `${tool}`
-             // liTool.innerHTML = `<li data-tools=${tool} class="item__require">${tool}  </li>`
-                addEventListenerLi(liTool)
-                document.querySelector(`#N${job.id}`).appendChild(liTool)
-            })
-        }
+    }
+}
 
-
+addEventListenerPostLi = (li) => {
+    li.addEventListener('click', function (event) {
+        const key = event.target.attributes[0].name
+        const data = event.target.attributes[0].value
+        updateFilterArray(key, data)
     })
 }
 
 
+updateFilterArray = (key, data) => {
+    /*Verifico que el dato seleccionado no este repetido*/
+    if (filterArray.filter(value => value[key] === data).length === 0) {
+        const filterObject = { [key]: data }
+        filtertDiv.classList.add('filter--show')
+        filterArray.push(filterObject)
+        addEventListenerClear(key, data)
+        createFilterLi(key, data)
+        updateJobsPosts(key, data)
+    }
+}
+
+
+/* Agrego evento Listener al buton Clear del filtro*/
+addEventListenerClear = (key, data) => {
+    document.querySelector('.button__clear').addEventListener('click', function () {
+        filtertDiv.classList.remove('filter--show')
+        filterUl.innerHTML = ''
+        filterArray.length = 0
+        //     jobPostArray.length = 0
+        jobPostArrayFiltered.length = 0
+        showJobPosts(jobPostArray)
+    })
+}
+
+createFilterLi = (key, data) => {
+    let li = document.createElement('li')
+    li.setAttribute(key, data)
+    let button = document.createElement('button')
+    button.classList.add('filter__closeBTN')
+    li.innerHTML = `${data}`
+    button.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+        <path fill="#FFF" fill-rule="evenodd"
+        d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
+        </svg>
+        `
+
+    /*Agrego el eventListener al button*/
+    button.addEventListener('click', function (e) {
+        deleteFilter(e)
+        showJobPosts(jobPostArrayFiltered)
+    })
+    /*Agrego al documento*/
+    li.appendChild(button)
+    filterUl.appendChild(li)
+}
+
+
+updateJobsPosts = (key, data) => {
+    let keyObject = key.substring(5) /*  role  = role-data  */
+
+    /* Verifico que jobPostArrayFiltered (array que contiene todos  los Posts Filtrados) este vacio. 
+    Si lo esta filtro  el jobPostArray que tiene todos los posts,
+    sino filtro el jobspostArrayfiltered*/
+    let jobPostArrayTemp = []
+
+    if (jobPostArrayFiltered.length === 0) {
+        jobPostArrayTemp = filterPost(jobPostArray, keyObject, data)
+    } else {
+        jobPostArrayTemp = filterPost(jobPostArrayFiltered, keyObject, data)
+    }
+
+    jobPostArrayFiltered.length = 0
+    jobPostArrayFiltered = [...jobPostArrayTemp]
+    showJobPosts(jobPostArrayFiltered)
+
+}
+
+
+
+filterPost = (array, key, data) => {
+    return array.filter(post => {
+        switch (key) {
+            case 'role':
+            case 'level':
+                if (post[key] && post[key] === data) {
+                    return true
+                }
+                break;
+            case 'skills':
+                if (post[key].includes(data)) {
+                    return true
+                }
+                break;
+            default:
+                break;
+        }
+    })
+}
+
+deleteFilter = (event) => {
+
+
+    let jobPostArrayTemp = []
+    let skillsArray = []
+    let roleObjectFilter = ''
+    let levelObjectFilter
+
+    eraseFilterElement(event) /* ejemplo:   role, "frontend" */
+    /*Filtro filterArray la seleccion */
+    filterArray = filterArray.filter(item => data !== item[key])
+
+    /*Recorro filterArray para separar datos, un array con skills y variables para role y level*/
+    filterArray.forEach(itemFilter => {
+        let propertyName = Object.getOwnPropertyNames(itemFilter)[0]
+
+        if (propertyName === "data-skills") {
+            skillsArray.push(itemFilter[propertyName])
+        } else if (propertyName === "data-role") {
+            roleObjectFilter = itemFilter[propertyName]
+        } else {
+            levelObjectFilter = itemFilter[propertyName]
+        }
+    })
+
+    jobPostArrayTemp = jobPostArray.filter(post => {
+        let isRoleRequested = false
+        let isLevelRequested = false
+        let isSkillsRequested = false
+
+         if (roleObjectFilter) {
+            if (roleObjectFilter === post.role) {
+                isRoleRequested = true
+            }
+        } else {
+            isRoleRequested = true
+        }
+
+         if (levelObjectFilter) {
+            if (levelObjectFilter === post.level) {
+                isLevelRequested = true
+            }
+        } else {
+            isLevelRequested = true
+        }
+
+        if (skillsArray) {
+            if (skillsArray.every(skill => post.skills.includes(skill))) {
+                isSkillsRequested = true
+            }
+        } else {
+            isSkillsRequested = true
+        }
+ 
+
+        if (isRoleRequested && isLevelRequested && isSkillsRequested) {
+             return true
+        }
+    })
+    console.log(jobPostArrayTemp)
+    jobPostArrayFiltered.length = 0
+    jobPostArrayFiltered = [...jobPostArrayTemp]
+}
+
+
+
+
+eraseFilterElement = (event) => {
+    switch (event.srcElement.tagName) {
+        case 'path':
+            event.target.parentNode.parentNode.parentNode.remove()
+            data = event.target.parentNode.parentNode.parentNode.attributes[0].value
+            key = event.target.parentNode.parentNode.parentNode.attributes[0].name
+            break;
+        case 'svg':
+            event.target.parentNode.parentNode.remove()
+            data = event.target.parentNode.parentNode.attributes[0].value
+            key = event.target.parentNode.parentNode.attributes[0].name
+
+            break;
+        case 'BUTTON':
+            event.target.parentNode.remove()
+            data = event.target.parentNode.attributes[0].value
+            key = event.target.parentNode.attributes[0].name
+            break;
+        default:
+            break;
+    }
+
+}
